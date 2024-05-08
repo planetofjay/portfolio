64ab3b9a51cca5394c45417c03ee579ba5bc5565
PlanetofJay <jae44dv@gmail.com>
2024-05-07 18:35:26 -0600

    screen and loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import Splash from "./components/screen/Splash";
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "./components/homepage/Home";
 import Main from "./pages/Main";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 
 export default function App() {
@@ -10,14 +13,23 @@ export default function App() {
     createRoutesFromElements(
       <Route path="/" element={<Home />}>
         <Route path="/" element={<Main />} />
-        
-      
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
     )
   )
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2800);
+  }, []);
+
   return (
     <>
+      {loading ? <Splash /> : (<RouterProvider router={router}/>)}
     </>
   );
-}
+};
