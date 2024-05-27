c619609e3dabe581312c2274d442e62052599d28
PlanetofJay <jae44dv@gmail.com>
2024-05-26 19:15:00 -0600

    update projects

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,27 +1,20 @@
 export const projects = [
   {
-    name: "Spotify",
-    projects: "Spotify Clone App",
-    year: "Personal, 2023.",
-    repo: "https://github.com/PlanetofJay/react-spotify",
-    url: "https://planetofjay.github.io/react-spotify/",
-  },
-  {
-    name: "PlanetOfJay",
-    projects: "Social Media App",
-    year: "Study Case, 2022.",
+    name: "PlanetOf",
+    projects: "Social Media Application",
+    year: "Study Case, 2022 - Current.",
     repo: "https://github.com/PlanetofJay/social-media-app1",
     url: "http://planetofjay.github.io/social-media-app1/",
   },
   {
-    name: "Weather",
+    name: "Weadget",
     projects: "Weather Component",
-    year: "Personal, 2022.",
+    year: "Personal 2022.",
     repo: "https://github.com/PlanetofJay/weather-app",
     url: "https://planetofjay.github.io/weather-app/",
   },
   {
-    name: "Notes",
+    name: "Noti",
     projects: "CRUD App & TodoList",
     year: "Personal, 2022.",
     repo: "https://github.com/PlanetofJay/my-note",
